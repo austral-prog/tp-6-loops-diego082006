e02930d0c10a9e4dbfbf2caf48b90fcdbe3a8651
diego082006 <ddelgado1@mail.austral.edu.ar>
2026-04-21 12:34:14 -0300

    Implement find_min, find_max, and count_negatives functions

--- find_extremes.py
+++ find_extremes.py
@@ -9,7 +9,13 @@ def find_min(numbers):
     Ejemplo: find_min([5, 5, 5]) -> 5
     Ejemplo: find_min([-3, -1, -7]) -> -7
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    minimo = numbers[0]
+    
+    for num in numbers:
+        if num < minimo:
+            minimo = num
+    
+    return minimo
 
 
 def find_max(numbers):
@@ -21,7 +27,13 @@ def find_max(numbers):
     Ejemplo: find_max([5, 5, 5]) -> 5
     Ejemplo: find_max([-3, -1, -7]) -> -1
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    maximo = numbers[0]
+    
+    for num in numbers:
+        if num > maximo:
+            maximo = num
+    
+    return maximo
 
 
 def count_negatives(numbers):
@@ -33,4 +45,10 @@ def count_negatives(numbers):
     Ejemplo: count_negatives([1, 2, 3]) -> 0
     Ejemplo: count_negatives([-1, -2, -3]) -> 3
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    contar_negativos = 0
+    
+    for num in numbers:
+        if num < 0:
+            contar_negativos += 1
+    
+    return contar_negativos
